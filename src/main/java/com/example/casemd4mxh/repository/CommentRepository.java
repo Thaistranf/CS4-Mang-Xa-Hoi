7497d83790d80e473f1052124d53ae75f579cc73
Thaistranf <anhnamduong9x9@gmail.com>
2023-12-04 16:20:40 +0700

    comment

--- src/main/java/com/example/casemd4mxh/repository/CommentRepository.java
+++ src/main/java/com/example/casemd4mxh/repository/CommentRepository.java
@@ -1,4 +1,13 @@
 package com.example.casemd4mxh.repository;
 
-public interface CommentRepository {
+import com.example.casemd4mxh.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Iterable<Comment> findAllByDescriptionContaining(String description);
+
+    Iterable<Comment> findAllByImageId(Long id);
+    void deleteByUserId(Long id);
 }
--- src/main/java/com/example/casemd4mxh/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/casemd4mxh/service/impl/CommentServiceImpl.java
@@ -1,4 +1,41 @@
 package com.example.casemd4mxh.service.impl;
 
-public class CommentServiceImpl {
+import com.example.casemd4mxh.model.Comment;
+import com.example.casemd4mxh.repository.CommentRepository;
+import com.example.casemd4mxh.service.CommentService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CommentServiceImpl implements CommentService {
+    @Autowired
+    CommentRepository commentRepository;
+    @Override
+    public Iterable<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+
+    @Override
+    public Iterable<Comment> findAllByImageId(Long id) {
+        return commentRepository.findAllByImageId(id);
+    }
+
+    @Override
+    public Optional<Comment> findById(Long id) {
+        return commentRepository.findById(id);
+    }
+
+    @Override
+    public void save(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
